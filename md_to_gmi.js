8edad9d31a276c1084945afbdffeb0283a0536ba
Nelson "Wasabi" C <nelson.cvlhr@gmail.com>
2023-01-27 16:04:26 +0100

    Create md_to_gmi.js

---
+++ md_to_gmi.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const marked = require('marked');
+
+// function to convert markdown to gemtext
+const convertMarkdownToGemtext = (markdown) => {
+    return marked(markdown, { renderer: new marked.Renderer() });
+};
+
+// get file name from command-line argument
+const fileName = process.argv[2];
+
+// read markdown file
+fs.readFile(fileName, 'utf8', (err, data) => {
+    if (err) throw err;
+
+    // convert markdown to gemtext
+    const gemtext = convertMarkdownToGemtext(data);
+
+    // get output file name by replacing the file extension
+    const outputFileName = fileName.replace(/\.md$/, '.gmi');
+
+    // write gemtext to a file
+    fs.writeFile(outputFileName, gemtext, (writeErr) => {
+        if (writeErr) throw writeErr;
+        console.log(`${fileName} converted to GEMText and saved as ${outputFileName}!`);
+    });
+});
